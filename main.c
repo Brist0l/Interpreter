19ce3d62430e8e8211e4b11c1d9b91bd02a9f105
Bris0l <gautamthegreat21@gmail.com>
2025-08-06 21:06:34 +0530

    Added INP instruction and added compare for two variables

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ const char* opcodes_str[] = {
 	"DIV", //Divide The Values
 	"POP", //Remove A value
 	"PRT", //Print The Stack
-	"HLT", //Terminate the Program
+	"HLT", //Terminate the Progvar
 	"SET", //Set values to variables
 	"INC", //Increase the value of variable by 1
 	"DEC", //Decrease the value of variable by 1
@@ -22,7 +22,8 @@ const char* opcodes_str[] = {
 	"LBL", //Define a label 
 	"JMP", //Jump to a specific label
 	"JNZ", //Jump to a specific label if zero flag is off 
-	"JZ" //Jump to a specific label if zero flag is on
+	"JZ", //Jump to a specific label if zero flag is on
+	"INP" //Take input into a variable from the user
 };
 
 typedef enum {
@@ -33,7 +34,7 @@ typedef enum {
 	DIV, //Divide The Values
 	POP, //Remove A value
 	PRT, //Print The Stack
-	HLT,  //Terminate the Program
+	HLT,  //Terminate the Progvar
 	SET,//Set values to variables
 	INC, //Increase the value of variable by 1
 	DEC, //Decrease the value of variable by 1
@@ -43,7 +44,8 @@ typedef enum {
 	LBL, //Define a label 
 	JMP,//Jump to a specific label
 	JNZ,//Jump to a specific label if zero flag is off 
-	JZ //Jump to a specific label if zero flag is on
+	JZ ,//Jump to a specific label if zero flag is on
+	INP //Take input into a variable from the user
 } opcodes;
 
 int ip = 0;  // instruction Pointer
@@ -51,11 +53,17 @@ int sp = 0; // stack Pointer
 int zf = 0; // zero flag
 int cf = 0; // carry flag
 int vr = 0; // var counter
-struct ram_hashmap{
+int lr = 0; // lable counter
+struct var_hashmap{
 	char vars[10];
 	int val;
 };
-struct ram_hashmap ram[10 * sizeof(struct ram_hashmap)]; 
+struct var_hashmap var[10 * sizeof(struct var_hashmap)]; 
+struct lable_hashmap{
+	char lables[10];
+	int inst_index;
+};
+struct lable_hashmap lable[10 * sizeof(struct lable_hashmap)]; 
 int stack[1024];
 bool running = true;
 char** program;
@@ -66,10 +74,19 @@ int readfile(char *filename, char **readarray,int readarray_size);
 int getopcode(char* opcode);
 bool checkstacklen(int des_len);
 int findvar();
+void gotolable();
+
+void gotolable(){
+	for(int i = 0; i < lr; i++)
+		if(strcmp(lable[i].lables,program[ip +1]) == 0){
+			ip = lable[i].inst_index;
+			break;
+		}
+}
 
 int findvar(){
 	for(int i = 0; i < vr; i++)
-		if(strcmp(ram[i].vars,program[ip +1]) == 0){
+		if(strcmp(var[i].vars,program[ip +1]) == 0){
 			ip++;
 			return i;
 		}
@@ -105,7 +122,7 @@ int readfile(char *filename, char **readarray,int readarray_size) {
 	int i = 0;
 	int j = 0;
 	while((ch = fgetc(fp)) != EOF){
-		//Reallocate the program array
+		//Reallocate the progvar array
 		if(i > readarray_size){
 			printf("Reallocating some mem cuz the array is smol\n");
 			readarray = realloc(readarray,readarray_size);
@@ -127,7 +144,7 @@ int readfile(char *filename, char **readarray,int readarray_size) {
 
 
 void eval(int instr) {
-	int a,b,var;
+	int a,b,var_cnt,var_cnt_alt;
 
 	if(sp >= 1024){
 		printf("Stack overflow !\n");
@@ -136,9 +153,9 @@ void eval(int instr) {
 	//printf("Instruction => %d\n",instr);
 	switch (instr){
 		case PSH: 
-			var = findvar();
-			if(var >= 0)
-				stack[sp++] = ram[var].val;
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				stack[sp++] = var[var_cnt].val;
 			else
 				stack[sp++] = atoi(program[++ip]);
 
@@ -177,9 +194,9 @@ void eval(int instr) {
 			break;
 
 		case POP: 
-			var = findvar();
-			if(var >= 0)
-				ram[var].val = stack[--sp];
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[var_cnt].val = stack[--sp];
 			else
 				printf("Popped %d\n", stack[--sp]);
 			break;
@@ -197,90 +214,116 @@ void eval(int instr) {
 			break;
 
 		case SET:
-			strcpy(ram[vr].vars, program[++ip]);
-			ram[vr++].val = atoi(program[++ip]);
-			//			printf("%s has value %d\n",ram[vr -1].vars,ram[vr -1].val);
+			strcpy(var[vr].vars, program[++ip]);
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[vr++].val = var[var_cnt].val;
+			else
+				var[vr++].val = atoi(program[++ip]);
+			//printf("%s has value %d\n",var[vr -1].vars,ram[vr -1].val);
 			break;
 		case INC:
-			var = findvar();
-			if(var >= 0)
-				ram[var].val += 1;
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[var_cnt].val += 1;
 			break;
 
 		case DEC:
-			var = findvar();
-			if(var >= 0)
-				ram[var].val -= 1;
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[var_cnt].val -= 1;
 			break;
 
 		case INCV:
-			var = findvar();
-			if(var >= 0)
-				ram[var].val += atoi(program[++ip]);
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[var_cnt].val += atoi(program[++ip]);
 			break;
 
 		case DECV:
-			var = findvar();
-			if(var >= 0)
-				ram[var].val -= atoi(program[++ip]);
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[var_cnt].val -= atoi(program[++ip]);
 			break;
 
 		case CMP:
-			for(a = 0; a < vr; a++)
-				if(strcmp(ram[a].vars,program[ip +1]) == 0){
-					ip++;
-					if(ram[a].val == atoi(program[ip + 1])){
-						ip++;
+			var_cnt = findvar();
+			if(var_cnt >= 0){
+				var_cnt_alt = findvar();	
+				if(var_cnt_alt >= 0){
+					//printf("Found another variable\n");
+					if(var[var_cnt].val > var[var_cnt_alt].val){
+						//printf("Checking %d > %d\n",var[var_cnt].val,var[var_cnt_alt].val);
+						//printf("varialbe is greater than num , zf=0 cf =0\n");
+						zf = 0;
+						cf = 0;
+					}
+					else if(var[var_cnt].val < var[var_cnt_alt].val){
+						//printf("Checking %d < %d\n",var[var_cnt].val,var[var_cnt_alt].val);
+						//printf("varialbe is smaller than num , zf=0 cf =1\n");
+						zf = 0;
+						cf = 1;
+					}
+					else if(var[var_cnt].val == var[var_cnt_alt].val){
+						//printf("Checking %d == %d\n",var[var_cnt].val,var[var_cnt_alt].val);
 						//printf("varialbe is equal to num , zf=1 cf =0\n");
 						zf = 1;
 						cf = 0;
 					}
-					else if(ram[a].val > atoi(program[ip + 1])){
+
+				}
+				else{
+					if(var[var_cnt].val > atoi(program[ip + 1])){
 						ip++;
 						//printf("varialbe is greater than num , zf=0 cf =0\n");
 						zf = 0;
 						cf = 0;
 					}
-					else if(ram[a].val < atoi(program[ip + 1])){
+					else if(var[var_cnt].val < atoi(program[ip + 1])){
 						ip++;
 						//printf("varialbe is smaller than num , zf=0 cf =1\n");
 						zf = 0;
 						cf = 1;
 					}
-
-					break;
+					else if(var[var_cnt].val == atoi(program[ip + 1])){
+						ip++;
+						//printf("varialbe is equal to num , zf=1 cf =0\n");
+						zf = 1;
+						cf = 0;
+					}
 				}
+
+			}
+
 			break;
 
 		case LBL:
-			strcpy(ram[vr].vars,program[++ip]); // save the label name in the ram
-			ram[vr++].val = ip;
+			strcpy(lable[lr].lables,program[++ip]); // save the label name in the ram
+			lable[lr++].inst_index = ip;
 			break;
 
 		case JMP:
-			for(a = 0; a < vr; a++)
-				if(strcmp(ram[a].vars,program[ip +1]) == 0){
-					ip = ram[a].val;
-					break;
-				}
+			gotolable();
 			break;	
 		case JNZ:
 			if(zf == 0)
-				for(a = 0; a < vr; a++)
-					if(strcmp(ram[a].vars,program[ip +1]) == 0){
-						ip = ram[a].val;
-						break;
-					}
+				gotolable();
 			break;	
 		case JZ:
 			// if zf is turned on 
 			if(zf == 1)
-				for(a = 0; a < vr; a++)
-					if(strcmp(ram[a].vars,program[ip +1]) == 0){
-						ip = ram[a].val;
-						break;
-					}
+				gotolable();
 			break;	
+		case INP:
+			scanf("%d",&a);
+			var_cnt = findvar();
+			if(var_cnt >= 0)
+				var[var_cnt].val = a;
+			else{
+				strcpy(var[vr].vars, program[++ip]);
+				var[vr++].val = a;
+			}
+
 	}
 }
 
@@ -299,7 +342,7 @@ int main(int argc, char *argv[]) {
 
 	program = prog;	
 
-	//printf("instrcution: %s and it's opcode = %d\n",program[ip],getopcode(program[ip]));
+	//printf("instrcution: %s and it's opcode = %d\n",progvar[ip],getopcode(program[ip]));
 
 	while(running){
 		eval(getopcode(program[ip]));
